bc3031d6e7c24bb7ef375587f1d255656400841f
Bobi <weplayallgames741@gmail.com>
2022-07-28 02:07:39 +0200

    Bulding Router pt2

--- src/Magma/Router/Router.php
+++ src/Magma/Router/Router.php
@@ -40,6 +40,45 @@ class Router implements RouterInterface
      */
     public function dispatch(string $url) :void
     {
+        if($this->match($url)){
+            $controllerSettings = $this->params['controller'];
+            $controllerSettings = $this->transformUpperCamelCase($controllerSettings);
+            $controllerSettings = $this->getNamespace($controllerSettings);
+        }
+    }
+
+    public function transformUpperCamelCase($controllerSettings)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', '', $controllerSettings)));
+    }
+
+
+    /**
+     * Match the route to the route in the routing table, setting the $this->params property 
+     * if a route is found
+     * 
+     * @param string $url
+     * @return bool 
+     */
+    public function match(string $url) : bool 
+    {
+        
+        foreach($this->routes as $route){
+            if(preg_match($route, $url, $matches)){
+                foreach($matches as $key => $param){
+                    if(is_string($key))
+                        $params[$key] = $param;
+                }
+                $this->params = $param;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public function getNamespace(string $string) : string
+    {
 
     }
 
